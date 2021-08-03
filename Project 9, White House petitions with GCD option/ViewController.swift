773bbd444643bc7cd42ccc8f5698060fb9649eda
Igor Polousov <igorpolousov@gmail.com>
2021-08-03 16:55:50 +0300

    + initial UI prepares

--- Project 9, White House petitions with GCD option/ViewController.swift
+++ Project 9, White House petitions with GCD option/ViewController.swift
@@ -1,19 +1,31 @@
 //
 //  ViewController.swift
 //  Project 9, White House petitions with GCD option
-//
+//  Day 39
 //  Created by Igor Polousov on 03.08.2021.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        return cell
+    }
+    
 }
 
