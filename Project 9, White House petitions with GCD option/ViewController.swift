91e5538371e8378b2a35d8625a5f926aff6342d3
Igor Polousov <igorpolousov@gmail.com>
2021-08-03 16:18:53 +0300

    Initial Commit

---
+++ Project 9, White House petitions with GCD option/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 9, White House petitions with GCD option
+//
+//  Created by Igor Polousov on 03.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
